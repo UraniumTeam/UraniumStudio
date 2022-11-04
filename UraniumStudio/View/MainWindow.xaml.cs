6593e9f6b563ca03999c6fb5a45fdbbe96c9c52c
MaiNotee <lapygin_d@mail.ru>
2022-11-04 16:28:00 +0500

    Added FileParser class with pregenerated functions, Renderer static class to convert functions to Canvas array. Canvas array binded to Canvas general panel and all elements of this array is rendering on panel. Refactored MainWindowVM class to follow MVVM pattern more.

--- UraniumStudio/View/MainWindow.xaml.cs
+++ UraniumStudio/View/MainWindow.xaml.cs
@@ -1,36 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using AvalonDock.Layout;
-using UraniumStudio.Model;
-using UraniumStudio.ViewModel;
 
 namespace UraniumStudio.View
 {
-
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public int Counter = 0;
-
 		public MainWindow()
 		{
-			
 			InitializeComponent();
-
 		}
 
 		void AddButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			var rnd = new Random();
+			/*var rnd = new Random();
 
 			var f = new Function(
 				rnd.Next(50, 250), 0, rnd.Next(50, 1500), "name" + rnd.Next(0, 100), Controller.GetRandomColor());
@@ -43,7 +27,8 @@ namespace UraniumStudio.View
 				Height = Function.Width,
 
 				RadiusX = 2,
-				RadiusY = 2
+				RadiusY = 2,
+				RenderedGeometry = {  }
 			};
 			var textBlock = new TextBlock()
 			{
@@ -55,7 +40,7 @@ namespace UraniumStudio.View
 			grid.Children.Add(rectangle);
 			grid.Children.Add(textBlock);
 			TreeView.Items.Add(grid);
-			//}
+			//}*/
 		}
 
 		void File_OnPreviewMouseLeftButtonDown(object sender, RoutedEventArgs e) =>
@@ -64,6 +49,11 @@ namespace UraniumStudio.View
 		void FileOpen_OnPreviewLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
 			var fileDialogWindow = new Microsoft.Win32.OpenFileDialog().ShowDialog();
+		}
+
+		void CanvasItem_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			MessageBox.Show("Canvas item click");
 		}
 	}
 }
---
+++ UraniumStudio/ViewModel/MainWindowVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using UraniumStudio.Data;
+
+namespace UraniumStudio.ViewModel;
+
+public class MainWindowVM
+{
+	public Canvas[] FunctionsToRender { get; }
+	
+	public MainWindowVM()
+	{
+		var fileParser = new FileParser();
+		FunctionsToRender = Renderer.GetGrids(fileParser.Functions);
+	}
+}
