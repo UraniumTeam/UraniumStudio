7993d455f13da20dd1142cd00d79925af78fe21a
MaiNotee <lapygin_d@mail.ru>
2022-12-08 19:31:53 +0500

    Created start window

---
+++ UraniumStudio/View/StartWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using UraniumStudio.Data;
+
+namespace UraniumStudio.View;
+
+public partial class StartWindow
+{
+	public StartWindow()
+	{
+		InitializeComponent();
+	}
+
+	void OpenExisting_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+	{
+		var dialog = new Microsoft.Win32.OpenFileDialog
+		{
+			Filter = "Uranium Profiler Session file|*.ups"
+		};
+		dialog.ShowDialog();
+		if (dialog.FileName == "") return;
+
+		Database.ThreadPaths = JsonParser.ParseUpsJson(dialog.FileName); // json UPS parser
+		var mainWindow = new MainWindow();
+		mainWindow.Show();
+		Hide();
+	}
+}
