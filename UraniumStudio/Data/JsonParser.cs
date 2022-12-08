245f403aa6b8ea97b3b9a977fe9e295fb6f529c5
MaiNotee <lapygin_d@mail.ru>
2022-12-08 18:56:12 +0500

    Implemented Json parser for *.ups files to get paths of *.upt files

---
+++ UraniumStudio/Data/JsonParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+
+namespace UraniumStudio.Data;
+
+public class JsonParser
+{
+	public static List<string> ParseUpsJson(string path)
+	{
+		List<string?> resultStrings = new();
+		using (var reader = new StreamReader(path))
+		{
+			string json = reader.ReadToEnd();
+			var threads = JObject.Parse(json);
+			var threadsValues = threads.Properties().Values();
+			resultStrings.AddRange(threadsValues.Select(value => value.ToString()));
+		}
+
+		return resultStrings!;
+	}
+}
