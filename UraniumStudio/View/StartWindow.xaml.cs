040ac692bcc69c9adbf6223fa77f78eedbc3f75b
Daniil.Lapygin <Daniil.Lapygin@urfu.me>
2022-12-29 15:51:00 +0500

    Worked on start window. Added icons. Improved stability.

--- UraniumStudio/View/StartWindow.xaml.cs
+++ UraniumStudio/View/StartWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Input;
 using UraniumStudio.Data;
 
@@ -6,31 +8,38 @@ namespace UraniumStudio.View;
 
 public partial class StartWindow
 {
-	public StartWindow()
-	{
-		InitializeComponent();
-	}
+    public StartWindow()
+    {
+        InitializeComponent();
+    }
 
-	void OpenExisting_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-	{
-		var dialog = new Microsoft.Win32.OpenFileDialog
-		{
-			Filter = "Uranium Profiler Session file|*.ups"
-		};
-		dialog.ShowDialog();
-		if (dialog.FileName == "") return;
+    void OpenExisting_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var dialog = new Microsoft.Win32.OpenFileDialog
+        {
+            Filter = "Uranium Profiler Session file|*.ups"
+        };
+        dialog.ShowDialog();
+        if (dialog.FileName == "") return;
 
-		Database.ThreadPaths = JsonParser.ParseUpsJson(dialog.FileName);
-		var mainWindow = new MainWindow();
-		mainWindow.Closing += MainWindowOnClosing;
-		mainWindow.Show();
-		Hide();
-	}
+        try
+        {
+            Database.ThreadPaths = JsonParser.ParseUpsJson(dialog.FileName);
 
-	void CreateNew_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-	{
-		
-	}
+            var mainWindow = new MainWindow();
+            mainWindow.Closing += MainWindowOnClosing;
+            mainWindow.Show();
+            Hide();
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show("Error: Wrong .UPS format file.");
+        }
+    }
 
-	void MainWindowOnClosing(object? sender, CancelEventArgs e) => Close();
+    void CreateNew_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+    }
+
+    void MainWindowOnClosing(object? sender, CancelEventArgs e) => Close();
 }
